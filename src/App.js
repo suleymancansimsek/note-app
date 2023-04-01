b8f0f0a998cfdc387a998373958926e41be687bc
suleymancansimsek <suleymancansimsek@gmail.com>
2023-04-02 00:27:24 +0300

    chagenging structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import SideBar from "./components/SideBar";
+import React from 'react'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="flex">
-      < SideBar />
-    </div>
-    
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
-
-
-
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCbX80J383rpbSox5HQOGlc9l4dvphhrSs",
-  authDomain: "noteapp-60fb1.firebaseapp.com",
-  projectId: "noteapp-60fb1",
-  storageBucket: "noteapp-60fb1.appspot.com",
-  messagingSenderId: "575411946952",
-  appId: "1:575411946952:web:049083ddea6d48a2d9ecfd"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig); 
-*/
+export default App
